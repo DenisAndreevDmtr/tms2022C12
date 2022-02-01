430f18fe0cddf28a3be151b0b588c68a28041442
User <https://github.com/DenisAndreevDmtr/tms2022C12.git>
2022-02-01 15:29:41 +0300

    Lesson6 Homework

---
+++ Lesson6/src/main/java/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        User user1 = new User("Denis", "Andreev", 27, "Male");
+        //      *  В Main классе в методе main создайте экземпляр класса User и вызовите все созданные методы, передав им необходимые параметры.
+        System.out.println(user1.typeNameSurname());
+        System.out.println(user1);
+        System.out.println(user1.increaseAge(2));
+        //     * 2) Создать в классе User несколько конструкторов. Создать объект этого класса, использовав один из них.
+        User user2 = new User("Ivan", "Petrov", 25);
+        System.out.println(user2);
+        //* Отметьте один из методов как @Deprecated. Попробуйте на экземпляре класса вызвать этот метод.
+        System.out.println(user1.increaseAge(2));
+    }
+
+}
