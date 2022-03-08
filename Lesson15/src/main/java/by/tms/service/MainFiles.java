257871d64d409b0cd4758f16abcaf5ce5d42b632
User <https://github.com/DenisAndreevDmtr/tms2022C12.git>
2022-03-08 19:43:18 +0300

    Lesson15

---
+++ Lesson15/src/main/java/by/tms/service/MainFiles.java
@@ -0,0 +1,12 @@
+package by.tms.service;
+
+import by.tms.module.HelperFile;
+
+import java.io.File;
+
+public class MainFiles {
+    public static void main(String[] args) {
+        File dir = new File("C://Users//User//IdeaProjects//tms2022C12//Lesson15//src//main//resources//folder123");
+        HelperFile.printAllFilesAndFolders(dir);
+    }
+}
