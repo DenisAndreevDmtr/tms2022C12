80e8e68f1a28a58493d8df68d510bc0b41cc612c
User <https://github.com/DenisAndreevDmtr/tms2022C12.git>
2022-03-16 17:32:06 +0300

    Lesson17 без 6 задания

---
+++ Lesson17/src/main/java/by/tms/module/Person.java
@@ -0,0 +1,14 @@
+package by.tms.module;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Person {
+    private String name;
+    private String surname;
+
+}
